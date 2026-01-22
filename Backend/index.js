8312b33d0d309e72222d528a93a8fe0d3deb50b4
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2026-01-22 12:12:05 +0530

    22/01/2026
    backend routes completed

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import db_Connection from './config/db.js';
-
+import authRouter from './routes/auth.routes.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 const app=express();
 const PORT=process.env.PORT || 30000;
- 
-app.get('/',(req,res)=>{
-    res.send('Hello World');
-})
+
+
+// middlewares
+app.use(express.json());
+app.use(cookieParser());//Reads cookies from request
+app.use("/api/auth",authRouter);//Registers authentication routes. Acts as a route prefix
+
 
 app.listen(PORT, async ()=>{
     await db_Connection();
